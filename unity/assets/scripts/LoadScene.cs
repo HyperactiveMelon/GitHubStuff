821f3b24c673d5f537f403640f84bf3fce7b4ebd
BigLad23 <60403958+BigLad23@users.noreply.github.com>
2020-02-15 12:13:33 +0200

    Add files via upload
    Siinä on uusi level selector ja aikasemmat scriptit uudessa kansiossa

---
+++ unity/assets/scripts/LoadScene.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadScene : MonoBehaviour
+{
+    public int nextSceneLoad;
+
+    void start()
+    {
+        nextSceneLoad = SceneManager.GetActiveScene().buildIndex + 1;
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            //Siirtyy seuraavaan tasoon¨
+            SceneManager.LoadScene(nextSceneLoad);
+
+            //INT Indexille
+            if(nextSceneLoad > PlayerPrefs.GetInt("levelAt"))
+            {
+                PlayerPrefs.SetInt("levelAt", nextSceneLoad);
+            }
+    }
+}
+}
