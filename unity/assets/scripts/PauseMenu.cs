65d78008afc6fe8c7b0b660609c03de6ed071ad4
BigLad23 <60403958+BigLad23@users.noreply.github.com>
2020-03-08 17:01:19 +0200

    Add files via upload

---
+++ unity/assets/scripts/PauseMenu.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool GameIsPaused = false;    
+
+    public GameObject  PauseUI;
+    
+
+    
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GameIsPaused)
+            {
+                Resume();
+            } 
+            else
+            {
+                Pause();
+            }
+        }
+    }
+   public void Resume ()
+    {
+        PauseUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+
+    void Pause ()
+    {
+        PauseUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+
+
+}
