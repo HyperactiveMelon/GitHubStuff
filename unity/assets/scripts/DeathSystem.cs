3ec66e5d57396635089a12d5afb23ff44249cb28
BigLad23 <60403958+BigLad23@users.noreply.github.com>
2020-03-20 14:53:16 +0200

    Add files via upload

---
+++ unity/assets/scripts/DeathSystem.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathSystem : MonoBehaviour
+{
+    public Transform respawn;
+    public Transform fallout;
+    void Start()
+    {
+        
+    }
+
+   
+    void Update()
+    {
+        
+    }
+      void OnTriggerEnter2D(Collider2D other) {
+        if(other.transform == fallout)
+        {
+            transform.position = respawn.position;
+    }
+}
+}
